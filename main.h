9ec4bbf767daf0f63896cb586a86902be5a2ffbc
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-24 17:50:04 -0700

    main.h file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+int pfunction_int(va_list args);
+int pfunction_dec(va_list args);
+int _putchar(char c);
+#endif
