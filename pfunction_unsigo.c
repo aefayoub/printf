358513b742bfeea91b968a713caf0e2854e8572f
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-26 16:28:07 -0700

    update

--- pfunction_unsigo.c
+++ pfunction_unsigo.c
@@ -4,8 +4,32 @@
  *
  *
  */
-int pfunction_oct(va_list num)
+int pfunction_oct(va_list args)
 {
-	unsigned int n = va_arg(num, unsigned int);
+	unsigned int n = va_arg(args, unsigned int);
 	int i = 0;
+	unsigned int num = n;
+	int *arr;
+	int count = 0;
+
+	while (n != 0)
+	{
+		n = n / 8;
+		count++;
+	}
+
+	array = malloc(count * sizeof(int));
+
+	for (i = 0; i < count; i++)
+	{
+		array[i] = num % 8;
+		num = num / 8;
+	}
+
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(arr[i] + '0');
+	}
+	free(arr);
+	return (count);
 }
--- pfunction_unsigx.c
+++ pfunction_unsigx.c
@@ -1,11 +1,36 @@
 #include "main.h"
 /**
+ * pfunction_hexlwc - prints unsigned it in hex lowercase.
+ * @
  *
- *
- *
+ * Rerurn;
  */
-int pfunction_hexlwc(va_list num)
+int pfunction_hexlwc(va_list args)
 {
-	unsigned int n = va_arg(num, unsigned int);
+	unsigned int n = va_arg(args, unsigned int);
 	int i = 0;
+	int count = 0;
+	unsigned int num = n;
+	int *arr;
+
+	while ( num != 0)
+	{
+		n = n / 16;
+		count++;
+	}
+	arr = malloc(count * sizeof(int));
+	for(i = 0; i < count; i++)
+	{
+		arr[i] = num % 16;
+		num = num / 16;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		if (arr[i] >9)
+			_putchar(arr [i] + 87);
+		else
+			_putchar(arr[i] + '0');
+	}
+	free(arr);
+	return count;
 }
