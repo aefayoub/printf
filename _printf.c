1f63601f250dc1d5ba0efbb007d8b2219db64aca
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-25 17:45:27 -0700

    hot update

--- _printf.c
+++ _printf.c
@@ -2,46 +2,46 @@
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
+ * @identif: specifier from the char*.
+ * @fun: ptr to specifier of the match. 
  * Return: the length of the string.
  */
 int _printf(const char * const format, ...)
 {
 	int i = 0;
+	int len = 0;
 	va_list num;
-	len = 0;
+	struct
+	{
+		int (*fun)(va_list);
+		const char *identif;
+	}
 	convert_match match[] = {
-		{"%s", pfunction_string}, {"%c", pfunction_char}, {"%i", pfunction_int},{"%d", pfunction_int}, {"%b", pfunction_bin},
+		{"%s", pfunction_string}, {"%c", pfunction_char}, {"%i", pfunction_int},
+		{"%d", pfunction_int}, {"%b", pfunction_bin},
 };
 va_start(num, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-while (format[i] != '\0')
+while (format[i] != format[i])
 {
 	if (format[i] == '%')
 	{
 		i++;
 		int j = 0;
+
 		while (j < sizeof(match) / sizeof(match[0]))
 		{
-			if (format[i] == match[j].id[1])
+			if (format[i] == match[j].identif[1])
 			{
-				len += match[j].f(num);
+				len += match[j].fun(num);
 				break;
 			}
 			j++;
 		}
 		if (j == sizeof(match) / sizeof(match[0]))
-		{
-			_putchar('%');
-			_putchar(format[i]);
-			len += 2;
-		}
+			len += _putchar('%') + _putchar(format[i]);
 	}
 	else
-	{
-		_putchar(format[i]);
-		len++;
-	}
+	leng += _putchar(format[i]);
 	i++;
 }
 }
