374c872c55498ad9805cb37dce5c0a70541dc548
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-26 20:05:28 -0700

    update

---
+++ print_unt.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_unt - prints an unsigned int
+ * @arguments: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int print_unt(va_list arguments, char *buf, unsigned int ibuf)
+{
+	unsigned int int_in, int_temp, i, div;
+	
+	int_in = va_arg(arguments, unsigned int);
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	if (int_in == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
+}
