bdf90b6054a999f9961387d1d6662e504da9b034
Ivyratermgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2023-06-28 06:29:33 +0200

    Update print_prg.c

--- print_prg.c
+++ print_prg.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include "main.h"
 /**
  * print_prg - Writes the character '%' to the buffer.
  * @a: Unused argument (to match the function pointer signature).
