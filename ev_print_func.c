e248ff0de6773acd5373b7a22ec51ccb1343bd36
aefayoub <aef.ayoub@gmail.com>
2023-07-01 06:02:17 -0700

    cmt changes

--- ev_print_func.c
+++ ev_print_func.c
@@ -11,7 +11,9 @@ int ev_print_func(const char *s, int index)
 		{"c", print_chr}, {"s", print_str}, {"i", print_int},
 		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
 		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
-	 
+		{"#i", print_int},{"#d", print_int},{"+o", print_oct},
+		{" o", print_oct},{"#u", print_unt},{"+u", print_unt},
+		{" u", print_unt},{"+x", print_hex},{" x", print_hex},
 		{NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
